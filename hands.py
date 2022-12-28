0bc2beea4416755a4c051630529db28632c99c65
Valentin <93974733+valentindush@users.noreply.github.com>
2022-12-28 11:04:32 +0200

    Hand module

--- hands.py
+++ hands.py
@@ -7,6 +7,11 @@ mpHands = mp.solutions.hands
 hands = mpHands.Hands()
 mpDraw = mp.solutions.drawing_utils
 
+#for FPS
+
+prev_time = 0
+curr_time = 0
+
 while True:
     success, frame = cap.read()
     imageRGB = cv.cvtColor(frame, cv.COLOR_BGR2RGB)
@@ -15,7 +20,19 @@ while True:
 
     if results.multi_hand_landmarks:
         for handLandmarks in results.multi_hand_landmarks:
+            for id, lm in enumerate(handLandmarks.landmark):
+                h, w, c = frame.shape
+                cx, cy = int(lm.x * w), int(lm.y * h)
+                if id == 6:
+                    cv.circle(frame, (cx,cy), 15, (255,0,255), cv.FILLED)
+
             mpDraw.draw_landmarks(frame,handLandmarks,mpHands.HAND_CONNECTIONS)
+    
+    curr_time = time.time()
+    fps = 1/(curr_time - prev_time)
+    prev_time = curr_time
+
+    cv.putText(frame, str(int(fps)),(10,70),cv.FONT_HERSHEY_SIMPLEX,1,(0,255,0),3)
 
     cv.imshow("video", frame)
 
