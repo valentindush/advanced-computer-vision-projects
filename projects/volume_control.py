21d3b684e6797cd43192957b892c7c56c566a7c9
Valentin <93974733+valentindush@users.noreply.github.com>
2022-12-30 19:34:47 +0200

    UPDT added pyCaw code

--- projects/volume_control.py
+++ projects/volume_control.py
@@ -17,6 +17,19 @@ ptime = 0
 
 detector = hd.hand_detector()
 
+ctypes import cast, POINTER
+from comtypes import CLSCTX_ALL
+from pycaw.pycaw import AudioUtilities, IAudioEndpointVolume
+
+devices = AudioUtilities.GetSpeakers()
+interface = devices.Activate(
+    IAudioEndpointVolume._iid_, CLSCTX_ALL, None)
+volume = cast(interface, POINTER(IAudioEndpointVolume))
+volume.GetMute()
+volume.GetMasterVolumeLevel()
+volume.GetVolumeRange()
+volume.SetMasterVolumeLevel(-20.0, None)
+
 while True:
     success, img = cap.read()
 
